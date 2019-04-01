e8c9d8edd81defcd64aa7216d9c679e771c6106a
kkstrive <kkstrive0124@gmail.com>
2019-04-01 15:56:27 +0800

    使用方式从npm改为git subtree

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# CHANGELOG
-
-## 1.2.0
-#### FIX
-过滤后使用shift多选，会将过滤之前的item选中
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import K2Transfer from './components/K2Transfer'
-
-module.exports.default = module.exports = {
-  K2Transfer,
-  install: function (Vue) {
-    Vue.component('K2Transfer', K2Transfer)
-  }
-}
